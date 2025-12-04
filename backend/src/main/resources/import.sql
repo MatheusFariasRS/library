3c7df35cfe59f3520c9d79c9f91f5aa35b9024ef
Matheus Farias <matheusaf.contato@gmail.com>
2025-12-04 12:54:12 -0300

    User Role model

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,6 +1,13 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
 
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
 INSERT INTO tb_genre(name) VALUES('Adventure');
 
 INSERT INTO tb_book(title, description) VALUES ('The Lord of the Rings', 'An epic fantasy adventure in Middle-earth, filled with magic, friendship, and the struggle between good and evil.');
